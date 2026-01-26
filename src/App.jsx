a0eb4c5c3f1c6f5e554c89950abd79b4e00bdabf
saravanan-pg <balusaravanabalu@gmail.com>
2026-01-26 16:59:43 +0530

    Add React source, Tailwind config, and Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-5xl font-extrabold text-green-400">
+        Job Portal
+      </h1>
+    </div>
+  );
+}
+
+export default App
